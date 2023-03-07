f76de37d88eb0dfd2d8cbbfc1aa2c890bd8b712f
Dmitry Pozdnyakov <2903015@gmail.com>
2023-03-07 19:58:33 +0300

    07-03-2023

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <div v-if="pending" class="loader">
+            <v-progress-circular indeterminate color="blue-grey"></v-progress-circular>
+        </div>
+
         <h1>{{ $t('index.title') }}</h1>
 
                 <!-- <v-container fluid style="margin: 10px 0 10px 0; padding: 0px;">
@@ -27,9 +31,12 @@
                     </v-row>                    
                 </v-container>     -->
 
+                
+
         <v-container fluid style="margin: 10px 0 10px 0; padding: 0px;">                
             <div v-for="item in images" :key="item.id" style="display: inline-block;">
                 <!-- <span>{{ item.download_url }}</span> -->
+                
                 <img :data-src="item.download_url == 'https://picsum.photos/id/1/5000/3333' ? '' : item.download_url" style="display: inline-block; width: 300px; height: 200px; border: 0px solid #fff; border-radius: 10px; margin: 5px;" alt="" title="" v-lazy-load>
                 <!-- <v-img :src="item.download_url == 'https://picsum.photos/id/1/5000/3333' ? '' : item.download_url" v-lazy-load 
                     style="border: 0px solid #fff; border-radius: 10px; margin: 5px;"
@@ -38,6 +45,7 @@
         </v-container>
         
         <!-- <iframe src="https://docs.google.com/document/d/e/2PACX-1vQNSJaBm0dGbcUsH4OHJzKWsY8vPjGeJXr4YzT-3lw8S5yx3FHvWbdHuOF23rHX9g/pub?embedded=true" style="border: 1px solid #ccc; border-radius: 10px; width: 100%; height: 720px; padding: 0; margin: 10px 0 0 0; " frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe> -->
+
     </div>
 </template>
 
@@ -72,5 +80,16 @@
     margin: 10px 10px 10px 0;
     padding: 10px;
 }
-
+.loader {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    z-index: 9999;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #CCCCCC77;
+}
 </style>
